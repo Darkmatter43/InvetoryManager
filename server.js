16744d0e2879d31020bbe459babeb9f4dd459a97
Yianni Catege <ycatege@gmail.com>
2021-12-08 14:55:11 -0500

    deploying to heroku

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 //took this from a tutorial on how to deploy to heroku - betterprogramming.pub
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
 const express = require('express');
+const path = require('path');
 const app = express();
-app.use(requireHTTPS);
-app.use(express.static('./dist/inventory-manager'));
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: './dist/inventory-manager'}
-  );
-  });
+app.use(express.static(__dirname + '/dist/inventory-manager'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/inventory-manager/index.html'));});
 app.listen(process.env.PORT || 8080);
